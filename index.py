ab82597e82f097eaf10ad6805398182f586b47a4
JoSEP <jeduardo8188@hotmail.com>
2024-09-13 17:02:08 -0500

    refactor:  format doc

--- index.py
+++ index.py
@@ -2,17 +2,14 @@ import tkinter as tk
 from tkinter import messagebox
 # import    pygetwindow as gw
 import pygetwindow as gw  # type: ignore
-import pyautogui # type: ignore
-from utils.func import get_window_icon, show_desktop, show_msg # type: ignore
+import pyautogui  # type: ignore
+from utils.func import get_window_icon, show_desktop, show_msg  # type: ignore
 
- 
 import win32gui
 import win32con
 import win32process
 import psutil
-from PIL import Image, ImageTk # type: ignore
-import win32ui
-
+from PIL import Image, ImageTk  # type: ignore
 
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
@@ -21,22 +18,22 @@ final_screen = int(screen_width * 0.4)
 # Calcular la posición x y y para centrar la ventana
 position_x = (screen_width - final_screen) // 2
 # position_y = (screen_height - window_height) // 2
-
 # Función para aumentar la altura al hacer hover
+
 def slide_down(event):
     root.geometry(f'{final_screen}x50+{position_x}+0')  # Cambia la altura a 50
-
 # Función para reducir la altura cuando se quite el hover
+
 def slide_up(event):
     root.geometry(f'{final_screen}x5+{position_x}+0')  # Cambia la altura a 5
-
 # Función para traer la ventana al frente
+
 def activate_window(hwnd):
     try:
-      #   win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)  # Si está minimizada, restaurarla
-      win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)  # Restaurar la ventana si está minimizada
-      win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)  # Maximizar la ventana
-      
+        #   win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)  # Si está minimizada, restaurarla
+        # Restaurar la ventana si está minimizada
+        win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+        win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)  # Maximizar la ventana
     except Exception as e:
         print(f"Error al activar la ventana: {e}")
 
@@ -44,7 +41,8 @@ def activate_window(hwnd):
 # Función para listar las ventanas activas
 def listar_aplicaciones_abiertas():
     def enum_windows_callback(hwnd, windows):
-        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd):  # Solo ventanas visibles con título
+        # Solo ventanas visibles con título
+        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd):
             _, pid = win32process.GetWindowThreadProcessId(hwnd)
             if psutil.pid_exists(pid):  # Verificar que el proceso exista
                 windows.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -62,14 +60,15 @@ def listar_aplicaciones_abiertas():
         icon_img = get_window_icon(hwnd)
 
         if icon_img:
-            icon_img = icon_img.resize((32, 32))  # Redimensionar el ícono
+            icon_img = icon_img.resize((30, 30))  # Redimensionar el ícono
             icon_tk = ImageTk.PhotoImage(icon_img)
             app_icon = tk.Label(taskbar_frame, image=icon_tk, bg='lightblue')
             app_icon.image = icon_tk  # Guardar la referencia para evitar que el ícono se destruya
-            app_icon.pack(side='left', padx=5)
-            app_icon.bind("<Button-1>", lambda event, hwnd=hwnd: activate_window(hwnd))
+            app_icon.pack(side='right', padx=5)
+            app_icon.bind("<Button-1>", lambda event,
+                          hwnd=hwnd: activate_window(hwnd))
     root.after(2000, listar_aplicaciones_abiertas)
-
+    
 
 root.geometry(f'{final_screen}x5+{position_x}+{0}')
 
@@ -78,29 +77,34 @@ root.configure(bg='#000000')
 root.attributes('-alpha', 0.8)
 root.attributes('-topmost', True)
 
+
 def on_taskbar_click(event):
-   show_desktop()
+    show_desktop()
+
 
 taskbar_frame = tk.Frame(root, bg='#000000', width=final_screen)
 taskbar_frame.pack(fill='both',)
 taskbar_frame.bind("<Button-1>", on_taskbar_click)
 
 # Crear otro frame que englobe tanto el taskbar como los íconos
-taskbar_wrapper = tk.Frame(root , bg='#ff0cde')
+taskbar_wrapper = tk.Frame(root, bg='#ff0cde')
 taskbar_wrapper.pack(fill='both', expand=True)
 
 # Asociar los eventos de hover a todo el área del taskbar y los íconos
-taskbar_wrapper.bind('<Enter>', slide_down)  # Cuando el ratón entra en el área visible
-taskbar_wrapper.bind('<Leave>', slide_up) 
-taskbar_frame.bind('<Enter>' , slide_down)
+# Cuando el ratón entra en el área visible
+taskbar_wrapper.bind('<Enter>', slide_down)
+taskbar_wrapper.bind('<Leave>', slide_up)
+taskbar_frame.bind('<Enter>', slide_down)
 # Crea botones o íconos para simular la barra de tareas
-button = tk.Button(taskbar_wrapper , text='Inicio', command=root.quit)
-button.pack(side='left',  )
+button = tk.Button(taskbar_wrapper, text='Inicio', command=root.quit)
+button.pack(side='left',)
 
 # Crear un botón que lleva al escritorio (minimiza todas las ventanas)
 
-desktop_button = tk.Button(taskbar_frame, text="Desktop", command=show_desktop ,
- background='#ec0d0d' , relief='flat' , height=50)
-desktop_button.pack(side='right',fill='y')
+desktop_button = tk.Button(taskbar_frame, text="Desktop", command=show_desktop,
+                           background='#ec0d0d', relief='flat', height=50)
+desktop_button.pack(side='right', fill='y')
 listar_aplicaciones_abiertas()
 root.mainloop()
+
+#  pyinstaller --onefile --noconsole index.py
